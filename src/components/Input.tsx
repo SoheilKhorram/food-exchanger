1210e88c634c50112d5114e61d094f84479e8fdc
SoheilKhorram <soheil13261428@gmail.com>
2023-08-16 14:42:42 +0430

    adding the input for the page

---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+interface inputProps {
+    className?: string,
+    type?: string,
+    placeholder?: string,
+    name?: string,
+    disabled?: boolean,
+    required?: boolean,
+    label: string
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
+    onClick?: (e: React.MouseEvent<HTMLInputElement>) => void,
+}
+
+const Input = ({ label, type, placeholder, name, disabled, className }: inputProps) => {
+    return (
+        <div className='flex flex-col items-end'>
+            <label className='text-primary-clr'>{label}</label>
+            <input className={`bg-dark-clr-400 text-primary-clr rounded-lg p-4 border border-solid focus-visible:border-primary-clr ${className}`} type={type} placeholder={placeholder} name={name} disabled={disabled} />
+        </div>
+    )
+}
+
+export default Input
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 import Button from '../components/Button'
 import photo from '../assets/imgs/1.jpg'
+import Input from '../components/Input'
 
 const HomePage = () => {
     return (
-        <>
-            <div className='grid grid-cols-2 grid-rows-2 gap-4 p-4'>
+        <div className='p-4'>
+            <div className='grid grid-cols-2 grid-rows-2 gap-4'>
                 <Button className='col-span-2 text-dark-clr-400 bg-primary-clr'>غذا بدم و غذا بگیرم</Button>
                 <Button className='text-dark-clr-400 bg-primary-clr'>فقط غذا بدم</Button>
                 <Button className='text-dark-clr-400 bg-primary-clr'>فقط غذا بگیرم</Button>
             </div>
-            <div className='flex flex-col items-end gap-3 p-4'>
+            <div className='flex flex-col items-end gap-3'>
                 <h3 className='text-primary-clr'>کدوم غذا؟</h3>
                 <div className="flex gap-2">
                     <div className='flex flex-col items-end p-2 rounded-lg text-primary-clr-100 bg-dark-clr-400'>
@@ -28,7 +29,8 @@ const HomePage = () => {
                     </div>
                 </div>
             </div>
-        </>
+            <Input className='w-full' label='کد فراموشی' />
+        </div>
     )
 }
 
